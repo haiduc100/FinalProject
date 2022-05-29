c5f63a8402599e8314635d4df00b13a05c258348
VanDuc <vanduc28062000@gmail.com>
2022-05-29 18:47:27 +0700

    update homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,11 @@
 const User = require("../models/user.model");
-
+const Asset = require("../models/asset.model");
 module.exports.getAllUser = async (req, res) => {
   try {
-    const user = await User.find({});
-    res.json(user);
+    const assets = await Asset.find({ State: "available" }).populate(
+      "Category"
+    );
+    res.render("pages/home", { listAsset: assets });
   } catch (error) {
     res.status(500).json({
       status: "fail",
