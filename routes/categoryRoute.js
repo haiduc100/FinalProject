89493dcf38309c58fc5dbfc1ce3ef3d2f2e5e9de
VanDuc <vanduc28062000@gmail.com>
2022-06-26 21:29:45 +0700

    change naming convention

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const controller = require("../controllers/categoryController");
 const CHECK_LOGIN = require('../middlewares/checkController') 
 
-
 router.get("/api", CHECK_LOGIN.checkLogin,controller.getAllCategories);
 router.post("/api",CHECK_LOGIN.checkLogin, controller.createCategory);
 router.put("/api/:id", CHECK_LOGIN.checkLogin,controller.updateCategory);
